0d7d4143b7079f12b1c0b02e801817ba96f308f4
lalala78622 <3122449703@qq.com>
2020-10-13 15:10:53 +0800

    sql

--- app/shared/io/db/strategy.ts
+++ app/shared/io/db/strategy.ts
@@ -52,7 +52,8 @@ export const insertTradeData = (strategy_id: string, trade_id: string) => {
     //window.alert("insertTradeData")
     //const date = require("silly-datetime")
     //let trading_day = date.format(new Date(),'YYYYMMDD')
-    let trading_day = "20201013"
+    //let trading_day = "20201013"
+    let trading_day = moment().format('YYYYMMDD');
     const addTime = +new Date().getTime() * Math.pow(10,6)
     return runInsertUpdateDeleteDB(LIVE_TRADING_DATA_DB, "INSERT INTO trades(trade_id,order_id,parent_order_id,trade_time,trading_day,instrument_id,exchange_id,source_id,account_id,client_id,instrument_type,side,offset,hedge_flag,price,volume,tax,commission) VALUES (?,'1','0', ?, ?, ?,'SZE','xtp','15015255', ?,'1','0','0','0','0.0','0','0','0')", [trade_id, addTime, trading_day, strategy_id, strategy_id])
 }
